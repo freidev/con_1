46169130c71237d49b1fc90394d4b5f0ea2036d1
freidev <freidev0607@gmail.com>
2026-05-19 14:58:41 -0300

    img5427

--- src/App.tsx
+++ src/App.tsx
@@ -5422,12 +5422,13 @@ export default function App() {
         <div className="flex h-full w-72 flex-col">
           <div className="border-b border-slate-800 px-5 py-5">
             {/* ESPAÇO PARA A LOGO — substitua por: <img src="/sua-logo.png" alt="Logo" className="h-full w-full object-contain" /> */}
-              <img 
+             
+            <div className="flex h-14 w-full items-center justify-center rounded-xl bg-white">
+               <img 
                 src="/sua-logo.png" 
                 alt="Logo" 
                 className="h-full w-full object-contain" 
                 />
-            <div className="flex h-14 w-full items-center justify-center rounded-xl bg-white">
             </div>
           </div>
 
